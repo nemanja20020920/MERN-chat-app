7c2b46aaea75017e1b65995eb048e14f15ae159a
Nemanja Djakovic <nemanjadjakovic.b@gmail.com>
2024-02-22 22:49:01 +0100

    Changed the error handler on backend a little.

--- api/app.js
+++ api/app.js
@@ -35,13 +35,12 @@ app.use(function (req, res, next) {
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+  });
 });
 
 module.exports = app;
